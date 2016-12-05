aea017bdbf8755a7c5c0560c8782b1190c72dadb
tony2times2times <tony.peraza86@gmail.com>
2016-12-05 15:26:09 -0600

    added ability to remove boxes and generally cleaned code up

--- scripts/client.js
+++ scripts/client.js
@@ -5,34 +5,66 @@ var yellow = 0;
 var green = 0;
 var blue = 0;
 $(document).ready(function functionName() {
-  console.log("Jquery standing by!");
-  $(".color-button").click(function(){
-    var color = $(this).data('color');
-    console.log('You have selected: '+color);
-    addBoxer(color);
-  });
+    console.log("Jquery standing by!");
+    enable();
 });
 
-function addBoxer(color) {
-  $('.container').append('<button class="color-cube '+color+'"></class>');
-
-  addCount(color);
+function enable() {
+    $(".color-button").unbind().on('click', addBoxer);
+    $(".color-cube").unbind().on('click', subtractBoxer);
 }
-function addCount(color){
-  if (color === 'red'){
-    red++;
-    $('#red').html('Total red: ' + red);
-  }
-  if (color === 'yellow'){
-    yellow++;
-    $('#yellow').html('Total yellow: ' + yellow);
-  }
-  if (color === 'green'){
-    green++;
-    $('#green').html('Total green: ' + green);
-  }
-  if (color === 'blue'){
-    blue++;
-    $('#blue').html('Total blue: ' + blue);
-  }
+
+
+function addBoxer() {
+    var color = $(this).data('color');
+    console.log('You have selected: ' + color);
+    $('.container').append('<button class="color-cube ' + color + '" data-color= '+
+    color+'></class>');
+    addCount(color);
+    enable();
+}
+
+function addCount(color) {
+    if (color === 'red') {
+        red++;
+        $('#red').html('Total red: ' + red);
+    }
+    if (color === 'yellow') {
+        yellow++;
+        $('#yellow').html('Total yellow: ' + yellow);
+    }
+    if (color === 'green') {
+        green++;
+        $('#green').html('Total green: ' + green);
+    }
+    if (color === 'blue') {
+        blue++;
+        $('#blue').html('Total blue: ' + blue);
+    }
+}
+
+function subtractBoxer() {
+    var color = $(this).data('color');
+    console.log('You have selected: ' + color);
+    $(this).remove();
+    subtractCount(color);
+}
+
+function subtractCount(color) {
+    if (color === 'red') {
+        red--;
+        $('#red').html('Total red: ' + red);
+    }
+    if (color === 'yellow') {
+        yellow--;
+        $('#yellow').html('Total yellow: ' + yellow);
+    }
+    if (color === 'green') {
+        green--;
+        $('#green').html('Total green: ' + green);
+    }
+    if (color === 'blue') {
+        blue--;
+        $('#blue').html('Total blue: ' + blue);
+    }
 }
